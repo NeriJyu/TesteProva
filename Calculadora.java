3e07fa275a419e4486e8dc7f3aa85e43f3c3ed57
NeriJyu <48592376+NeriJyu@users.noreply.github.com>
2020-04-12 19:33:35 -0300

    Arquivo Java Teste
    1º upload
    1ª versão

---
+++ Calculadora.java
@@ -0,0 +1,37 @@
+
+public class Calculadora {
+	
+	
+	private int resultado = 0;
+	
+	public int somar(int n1, int n2) {
+		
+		resultado = n1 + n2;
+		
+		return resultado;
+	}
+
+	
+	public int subtracao(int n1, int n2) {
+		resultado = n1 - n2;
+		
+		return resultado ;
+	}
+	
+	public int multiplicacao(int n1, int n2) {
+		resultado = n1 * n2;
+		
+		return resultado ;
+	}
+	
+	public int divisao(int n1, int n2) {
+		resultado = n1 / n2;
+		
+		return resultado ;
+	}
+
+	
+	
+	
+
+}
